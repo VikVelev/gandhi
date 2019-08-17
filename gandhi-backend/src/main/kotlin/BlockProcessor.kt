3bb162f9560e047a3b0e324ba8c7881f048b698b
alex4o <mrabonin@gmail.com>
2019-08-17 12:40:13 +0300

    service block add

--- gandhi-backend/src/main/kotlin/BlockProcessor.kt
+++ gandhi-backend/src/main/kotlin/BlockProcessor.kt
@@ -1,10 +1,38 @@
+import Data.Block
 import org.apache.ignite.services.Service
 import org.apache.ignite.services.ServiceContext
+import org.apache.ignite.Ignite
+import org.apache.ignite.IgniteQueue
+import org.apache.ignite.configuration.CollectionConfiguration
+import org.apache.ignite.lang.IgniteFuture
+import org.apache.ignite.resources.IgniteInstanceResource
+import java.math.BigInteger
+import java.util.concurrent.CompletableFuture
+
+
+fun <V> IgniteFuture<V>.toCompletableFuture(): CompletableFuture<V> {
+	val future = CompletableFuture<V>()
+	this.listen { fut ->
+		try {
+			val res = fut.get();
+			future.complete(res);
+		}catch(e: Exception){
+			future.completeExceptionally(e);
+		}
+	};
+	return future;
+}
+
 
 class BlockProcessor : Service {
 
-	override fun init(ctx: ServiceContext?) {
+	@IgniteInstanceResource
+	lateinit var ignite: Ignite
 
+	lateinit var queue: IgniteQueue<Block>
+
+	override fun init(ctx: ServiceContext?) {
+		queue = ignite.queue<Block>("PendingBlocks", 0, CollectionConfiguration())
 	}
 
 	override fun cancel(ctx: ServiceContext?) {
@@ -12,7 +40,31 @@ class BlockProcessor : Service {
 	}
 
 	override fun execute(ctx: ServiceContext?) {
+		while (!ctx!!.isCancelled) {
+			var block = queue.poll()
 
+			val results = CompletableFuture.allOf(
+				*block.transactions.map {
+					ignite.compute().affinityCallAsync(DataStore.balances.name, it.key) {
+
+						for (transfer in it.value.transfers) {
+							
+						}
+
+					}.toCompletableFuture()
+				}.toTypedArray()
+			)
+
+
+
+			println("Block: ${block.number}")
+
+
+		}
+	}
+
+	fun addBlock(block: Block) {
+		queue.put(block)
 	}
 
 }
