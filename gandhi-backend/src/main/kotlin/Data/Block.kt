39c5697d1200c339892c4f61ac37e65e79998c7e
zvezdin <git@zvezd.in>
2019-08-17 13:15:38 +0300

    Add basic setter/getter API

--- gandhi-backend/src/main/kotlin/Data/Block.kt
+++ gandhi-backend/src/main/kotlin/Data/Block.kt
@@ -2,4 +2,4 @@ package Data
 
 import java.util.Date
 
-data class Block(val number: Int, val timestamp: Date, val author: Address, val transactions: Map<Address, Transaction>)
+data class Block(val number: Long, val timestamp: Date, val author: Address, val transactions: Map<Address, Transaction>)
--- gandhi-backend/src/main/kotlin/Web/routes.kt
+++ gandhi-backend/src/main/kotlin/Web/routes.kt
@@ -7,6 +7,7 @@ import io.ktor.application.install
 import io.ktor.auth.UserPasswordCredential
 import io.ktor.request.receive
 import io.ktor.response.respond
+import io.ktor.response.respondText
 import io.ktor.routing.*
 import org.apache.ignite.cache.query.ScanQuery
 import java.util.*
@@ -19,6 +20,25 @@ fun Application.routes() {
         get("/") {
 
             call.respond(DataStore.blocks.query(ScanQuery<Long, Block>()).map { Pair(it.key, it.value) })
+
+        }
+
+        route("blocks") {
+
+            get("/{id}") {
+
+                call.respond(DataStore.blocks.get(call.parameters["id"]!!.toLong()))
+            }
+
+            post("/submit") {
+                val block = call.receive<Block>()
+
+
+                DataStore.blocks.put(block.number, block);
+
+                call.respondText("post received")
+
+            }
 
         }
 
