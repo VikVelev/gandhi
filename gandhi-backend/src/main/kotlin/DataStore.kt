2ebb8d4019e3cd4b74b3da60b6655f0c637bcdce
alex4o <mrabonin@gmail.com>
2019-08-17 18:07:04 +0300

    All caches are replicated now

--- gandhi-backend/src/main/kotlin/DataStore.kt
+++ gandhi-backend/src/main/kotlin/DataStore.kt
@@ -2,7 +2,9 @@ import Data.*
 import org.apache.ignite.IgniteCache
 import org.apache.ignite.IgniteDataStreamer
 import org.apache.ignite.Ignition
+import org.apache.ignite.cache.CacheMode
 import org.apache.ignite.cache.query.ScanQuery
+import org.apache.ignite.configuration.CacheConfiguration
 import org.apache.ignite.configuration.DataStorageConfiguration
 import org.apache.ignite.stream.StreamVisitor
 import java.math.BigInteger
@@ -12,10 +14,19 @@ import kotlin.random.Random
 
 
 object DataStore {
+    var blo_cfg = CacheConfiguration<Long, Block>("Blocks")
+    var bal_cfg = CacheConfiguration<String, BigInteger>("Balances")
 
     val ignite = Ignition.start(igniteConfiguration {
         igniteHome = Paths.get("ignite").toAbsolutePath().toString()
         isPeerClassLoadingEnabled = true
+
+        blo_cfg.setCacheMode(CacheMode.REPLICATED)
+        bal_cfg.setCacheMode(CacheMode.REPLICATED)
+
+        setCacheConfiguration(blo_cfg)
+        setCacheConfiguration(bal_cfg)
+
 //        dataStorageConfiguration = DataStorageConfiguration()
 //        dataStorageConfiguration.defaultDataRegionConfiguration.setPersistenceEnabled(true)
     })
@@ -31,7 +42,7 @@ object DataStore {
 
     var blocks: IgniteCache<Long, Block> = ignite.getOrCreateCache("Blocks")
 
-    var senders: IgniteCache<Pair<Long, BigInteger>, Boolean> = ignite.getOrCreateCache("Senders")
+//    var senders: IgniteCache<Pair<Long, BigInteger>, Boolean> = ignite.getOrCreateCache("Senders")
 
     var balances: IgniteCache<String, BigInteger> = ignite.getOrCreateCache("Balances")
 
