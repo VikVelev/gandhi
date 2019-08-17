830a6452d206abb09b69fbe5e45e5fa8a02039c7
Viktor Velev <viktorvelev8@gmail.com>
2019-08-17 11:22:07 +0300

    React boilerplate fixed

---
+++ gandhi-frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                HI
+            </div>
+        );
+    }
+}
+ 
+export default Navbar;
